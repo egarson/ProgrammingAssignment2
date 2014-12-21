873c1455dc71bbf96b8f6098c2ec2e758821a415
Edward Garson <egarson@gmail.com>
2014-12-21 14:50:10 -0500

    minor improvements

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,13 @@
-## Coursera "R Programming", Assignment 2.
+#!/bin/R
+##
+## Coursera "R Programming" course, Assignment 2.
 ##
 ## Author: Edward Garson
 ## Date: December 2014
 ##
-## https://class.coursera.org/rprog-016/human_grading/view/courses/972581/assessments/3/submissions
+## Further info:
+##   https://class.coursera.org/rprog-016/human_grading/view/courses/972581/assessments/3/submissions
+
 
 ## Return a list object that encapsulates a given matrix and its calculated inverse.
 ## `x_inv' is the memoized variable that caches the inverse
@@ -19,32 +23,8 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Returns the inverse of `x$matrix', potentially retrieving it from cache
 cacheSolve <- function(x, ...) {
     if (!is.null(x$inverse())) {
-        # message("cache hit")
+        message("cache hit")
         return(x$inverse())
     }
     x$cache(solve(x$matrix, ...)) # n.b. this *returns* the inverse in addition to caching it
-}
-
-makeVector <- function(x = numeric()) {
-    m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
-    }
-    get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
-    list(set = set, get = get, setmean = setmean, getmean = getmean)
-}
-
-cachemean <- function(x, ...) {
-    m <- x$getmean()
-    if(!is.null(m)) {
-        message("getting cached data")
-        return(m)
-    }
-    data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
-    m
 }
